c857ca38b6e5acc053fcb67361488746ef5cf14f
Connor <90352499+Connor-vb@users.noreply.github.com>
2022-02-21 14:44:08 -0800

    working on nav functionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,13 @@ function App() {
       <header>
         {/* <h1>Swap Buddy</h1> */}
 
-
+{/* header */}
 <nav class="navbar navbar-expand-lg">
   <h1 class="navbar-brand">Swap Buddy</h1>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <i class="fa-regular fa-bars"></i>
   </button>
-
-
-
+{/* header hamburger drop dropdown */}
  <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -34,14 +32,7 @@ function App() {
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
