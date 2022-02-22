44340f0ac576813f9c9cb1f1ded98a2b9e1bd61b
Connor <90352499+Connor-vb@users.noreply.github.com>
2022-02-21 16:34:52 -0800

    fixed the dropdown's functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,50 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
   return (
-    <div className="App font-link">
-      <header>
-        {/* <h1>Swap Buddy</h1> */}
-
-{/* header */}
-<nav class="navbar navbar-expand-lg">
-  <h1 class="navbar-brand">Swap Buddy</h1>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <i class="fa-regular fa-bars"></i>
-  </button>
-{/* header hamburger drop dropdown */}
- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-      </header>
-      <section>
-        <nav className="nav-search">
-        
-        <form>
-        <div>
-          <label for="search">Already know what you want?</label>
-        </div>
-          <input type="text" id="search" name="search" placeholder="Search"></input>
-          <button>Search</button>
-        </form>
-        </nav>
-      </section>
+  <body className="font-link">
+    <header>
+    <Navbar collapseOnSelect expand="lg" className="nav-login">
+      <Navbar.Brand href="#home" >
+      <h1>
+        Swap Buddy
+      </h1>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#messages" className="link">Messages</Nav.Link>
+          <Nav.Link href="#login" className="link">Login</Nav.Link>
+          <Nav.Link href="#signup" className="link">Sign-up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    </header>
+    <nav>
+      <Navbar collapseOnSelect expand="lg" className="nav-search">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" className="nav-bar-btn"> <h5>All Categories</h5> </Navbar.Toggle>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto dropdown-menu">
+          <Nav.Link href="#messages" className="link">Category</Nav.Link>
+          <Nav.Link href="#login" className="link">Category</Nav.Link>
+          <Nav.Link href="#signup" className="link">Category</Nav.Link>
+          <Nav.Link href="#signup" className="link">Category</Nav.Link>
+          <Nav.Link href="#signup" className="link">Category</Nav.Link>
+          <Nav.Link href="#signup" className="link">Category</Nav.Link>
+          <Nav.Link href="#signup" className="link">Category</Nav.Link>
+          <Nav.Link href="#signup" className="link">Category</Nav.Link>
+          <Nav.Link href="#signup" className="link">Category</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Brand href="#home" >
       
-    </div>
+      </Navbar.Brand>
+    </Navbar>
+    </nav>
+  </body>
   );
 }
-
-export default App;
