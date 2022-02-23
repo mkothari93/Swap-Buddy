68636e875f2ff243086903d6ecad6f70ac638a25
Connor <90352499+Connor-vb@users.noreply.github.com>
2022-02-22 20:38:44 -0800

    fixed layout and responsiveness for cards on main page

--- client/src/App.js
+++ client/src/App.js
@@ -52,7 +52,7 @@ export default function App() {
     <main> 
     <div className="container"> 
     <div className="row">
-    <div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+    <div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -74,7 +74,7 @@ export default function App() {
 </Card>
 </div>
 {/* card 2 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+<div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -97,7 +97,7 @@ export default function App() {
 </div>
 
 {/* card 3 */}
-    <div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+    <div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -119,7 +119,7 @@ export default function App() {
 </Card>
 </div>
 {/* card 4 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+<div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -141,7 +141,7 @@ export default function App() {
 </Card>
 </div>
 {/* card 5 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+<div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -163,7 +163,7 @@ export default function App() {
 </Card>
 </div>
 {/* card 6 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+<div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -185,8 +185,8 @@ export default function App() {
 </Card>
 </div>
 
-{/* card 7 */}
-    <div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+ {/* card 7 */}
+    {/* <div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -206,9 +206,9 @@ export default function App() {
     <Card.Link href="#">Another Link</Card.Link>
   </Card.Body>
 </Card>
-</div>
+</div> */}
 {/* card 8 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
+{/* <div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
@@ -228,7 +228,29 @@ export default function App() {
     <Card.Link href="#">Another Link</Card.Link>
   </Card.Body>
 </Card>
-</div>
+</div> */}
+{/* card 9 */}
+{/* <div className="col-sm-12 col-md-4 col-lg-4 m-auto d-flex justify-content-center" id="card">
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Cras justo odio</ListGroupItem>
+    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+</div> */}
 </div>
 </div>
 </main>
