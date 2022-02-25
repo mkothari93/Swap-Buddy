985f04c0db5317c2eb056c21225aa5f9094766dd
greenlight510 <90018321+greenlight510@users.noreply.github.com>
2022-02-24 19:24:01 -0800

    Signup Login Home and some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,237 +1,72 @@
 import React from "react";
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Nav, Navbar, NavDropdown, Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/home.component";
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
-export default function App() {
+function App() {
   return (
-    // nav
-  <body className="font-link">
-    <header>
-    <Navbar collapseOnSelect expand="xxxl" className="nav-login" sticky="top">
-      <Navbar.Brand href="#home" >
-      <h1>
-        Swap Buddy
-      </h1>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto dropdown-login">
-          <Nav.Link href="#messages" className="link">Messages</Nav.Link>
-          <Nav.Link href="#login" className="link">Login</Nav.Link>
-          <Nav.Link href="#signup" className="link">Sign-up</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    </header>
-    <nav>
-      <Navbar collapseOnSelect expand="xxxl" className="nav-search" >
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="nav-bar-btn"> <h5>All Categories</h5> </Navbar.Toggle>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto dropdown-menu">
-          <Nav.Link href="#messages" className="link">Category</Nav.Link>
-          <Nav.Link href="#login" className="link">Category</Nav.Link>
-          <Nav.Link href="#signup" className="link">Category</Nav.Link>
-          <Nav.Link href="#signup" className="link">Category</Nav.Link>
-          <Nav.Link href="#signup" className="link">Category</Nav.Link>
-          <Nav.Link href="#signup" className="link">Category</Nav.Link>
-          <Nav.Link href="#signup" className="link">Category</Nav.Link>
-          <Nav.Link href="#signup" className="link">Category</Nav.Link>
-          <Nav.Link href="#signup" className="link">Category</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-      <Navbar.Brand href="#home" >
-      
-      </Navbar.Brand>
-    </Navbar>
-    </nav>
-    {/* nav ends */}
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/"}>
+              Swap Buddy
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          
+        </nav>
+        <nav id="categorynav">
+        <Navbar collapseOnSelect expand="xxxl" className="nav-search" >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="nav-bar-btn"> <h5>All Categories</h5> </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto dropdown-menu">
+            <Nav.Link href="#messages" className="link">Category</Nav.Link>
+            <Nav.Link href="#login" className="link">Category</Nav.Link>
+            <Nav.Link href="#signup" className="link">Category</Nav.Link>
+            <Nav.Link href="#signup" className="link">Category</Nav.Link>
+            <Nav.Link href="#signup" className="link">Category</Nav.Link>
+            <Nav.Link href="#signup" className="link">Category</Nav.Link>
+            <Nav.Link href="#signup" className="link">Category</Nav.Link>
+            <Nav.Link href="#signup" className="link">Category</Nav.Link>
+            <Nav.Link href="#signup" className="link">Category</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Brand href="#home" >
+        
+        </Navbar.Brand>
+      </Navbar>
+      </nav>
+        <div className="auth-wrapper">
+          
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+            </Switch>
+         </div>
+      </div>
+    </Router>
 
-    {/* <main> with pictures begins */}
-    <main> 
-    <div className="container"> 
-    <div className="row">
-    <div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
-{/* card 2 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
 
-{/* card 3 */}
-    <div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
-{/* card 4 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
-{/* card 5 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
-{/* card 6 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
-
-{/* card 7 */}
-    <div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
-{/* card 8 */}
-<div className="col-sm-12 col-md-6 col-lg-4 m-3" id="card">
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-</div>
-</div>
-</div>
-</main>
-  </body>
-  );
+     );
 }
+
+export default App;
