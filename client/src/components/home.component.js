f4cc07c8d53e1cf75310b2039d69df66cca8c57b
Kate Hamilton <kate.hamilton828@gmail.com>
2022-02-28 21:03:50 +0300

    update

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -11,16 +11,15 @@ export default class Home extends Component {
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>book</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+     Harry Potter series 
     </Card.Text>
   </Card.Body>
   <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+    <ListGroupItem>New Condition</ListGroupItem>
+    <ListGroupItem>New York,New York</ListGroupItem>
+   
   </ListGroup>
   <Card.Body>
     <Card.Link href="#">Card Link</Card.Link>
